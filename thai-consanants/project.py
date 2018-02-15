ee87dc59aadc34aabdcc8e6d6ccb8697cdeb2322
Kennedy DeSousa <kennedydesousa@gmail.com>
2018-02-15 10:34:43 -0800

    Added Flask

---
+++ thai-consanants/project.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello(name=None):
+    return render_template('index.html', name=name)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5050)
