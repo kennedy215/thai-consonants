8f0f70fde4d5071984e3030f0e51ab1eacca76fe
Kennedy DeSousa <kennedydesousa@gmail.com>
2018-02-20 07:29:07 -0800

    corrected definition

--- thai-consanants/project.py
+++ thai-consanants/project.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import render_template
 
+# One way of connecting css file
+# app = Flask(__name__,static_url_path='/css')
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/<string:name>')
 def hello(name=None):
     return render_template('index.html', name=name)
 
